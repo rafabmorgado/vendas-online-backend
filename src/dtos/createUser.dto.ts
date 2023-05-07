73179375d8868206171d1ea7532b96fb9bdf9629
Rafael Morgado <rafabmorgado@gmail.com>
2023-05-07 13:54:24 -0300

    feat: create user (POST)

---
+++ src/dtos/createUser.dto.ts
@@ -0,0 +1,7 @@
+export interface CreateUSerDto {
+  name: string;
+  email: string;
+  phone: string;
+  cpf: string;
+  password: string;
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUSerDto } from 'src/dtos/createUser.dto';
 
 @Controller('user')
 export class UserController {
-  @Get()
-  async getAllUsers() {
-    return JSON.stringify({ test: 'abc' });
+  //   @Get()
+  //   async testeApi() {
+  //     return JSON.stringify({ test: 'abc' });
+  //   }
+
+  @Post()
+  async createUser(@Body() createUser: CreateUSerDto) {
+    return createUser;
   }
 }
